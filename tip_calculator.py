020064f5284b703dd037c1dad3e67c3b71af3dff
Britney Wilson <britneykayewilson@gmail.com>
2017-06-02 21:16:29 +0000

    Homework 1 Done

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,7 +1,12 @@
-
-tip = 20.0
-total = 30.0
-server = 'John'
+print "What is the name of your server?",
+name = raw_input()
+print "What is the amount of your meal?: $",
+meal = float(raw_input())
+print "What is the tax rate?",
+tax = (.01*float(raw_input()))*meal
+print "What percentage do you want to tip?",
+tip = (.01*int(raw_input()))*meal
+total = meal+tax+tip
 print "The tip amount is: $" + str(tip)
 print "The total amount for my meal is: $" + str(total)
-print "Thanks for your service " + server
+print "Thanks for your service " + name
